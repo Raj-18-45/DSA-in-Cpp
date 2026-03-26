590ce8bc33b6290bc5342edb3b14575180a03118
Raj-18-45 <sumitrajofficial1010@gmail.com>
2026-03-26 18:42:21 +0530

    Update linear_search.cpp

--- Arrays/linear_search.cpp
+++ Arrays/linear_search.cpp
@@ -1,1 +1,43 @@
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int linearSearch(vector<int> nums, int target){
+    int n = nums.size();
+    
+    for(int i = 0; i < n; i++){
+        if(nums[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> v = {1,2,3,4,5,6};
+    v.push_back(7);
+
+    int target;
+    cout << "Enter your target: ";
+    cin >> target;
+
+    int validate = linearSearch(v, target);
+
+    if(validate == -1){
+        cout << "Not found";
+    }
+    else{
+        cout << "Found at index " << validate;
+    }
+
+    return 0;
+}
+
+//  Time Complexity:
+// 	•	Best: O(1)
+// 	•	Worst: O(n)
+
+//  Space Complexity:
+// 	•	Current code: O(n)
+// 	•	Optimized code: O(1)
